219b3acf1a2d7d58723738fa8407f888ce45cdd8
Seneen Khan <khan.seneen333@gmail.com>
2025-07-24 20:15:00 +0530

    Changed route

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173','https://whobotbetter.vercel.app' ],
+  origin: ['http://localhost:5173','https://whobotbetter/compare.vercel.app' ],
   credentials: true
 }));
 app.use(express.json());
