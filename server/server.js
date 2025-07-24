58695887962034030685bda882d7ca6a8c22aaa2
Seneen Khan <khan.seneen333@gmail.com>
2025-07-24 21:57:09 +0530

    Additional cors added

--- server/server.js
+++ server/server.js
@@ -5,15 +5,34 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
+const allowedOrigins = [
+  'http://localhost:5173', 
+  'https://whobotbetter.vercel.app', 
+  'https://whobotbetter-*.vercel.app' 
+];
+
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://whobotbetter.vercel.app',
-    'https://whobotbetter-*.vercel.app'
-  ],
-  credentials: true
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.some(allowedOrigin => 
+      origin === allowedOrigin || 
+      origin.match(/^https:\/\/whobotbetter-.*\.vercel\.app$/) 
+    )) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  maxAge: 86400 
 }));
-app.use(express.json());
+app.options('*', cors());
+app.use(express.json({ limit: '10mb' })); 
+app.use(express.urlencoded({ extended: true }));
+
 import claudeRoute from './routes/claudeRoute.js';
 import geminiRoute from './routes/geminiRoute.js';
 import openaiRoute from './routes/openaiRoute.js';
