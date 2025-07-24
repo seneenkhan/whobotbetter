c08f96d5e7f90e7da62f715aff6db03beae547f9
Seneen Khan <khan.seneen333@gmail.com>
2025-07-24 20:50:53 +0530

    Vercel link

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:5173','https://whobotbetter/compare.vercel.app' ],
+  origin: ['http://localhost:5173','https://whobotbetter-p410jg1xs-seneenkhans-projects.vercel.app' ],
   credentials: true
 }));
 app.use(express.json());
